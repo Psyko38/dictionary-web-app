8fa99b7cee9d3268a0dd02d857e72ea0b0b44451
Psyko38 <110417117+Psyko38@users.noreply.github.com>
2025-04-11 16:32:06 +0200

    feat app : final app

--- index.js
+++ index.js
@@ -1,4 +1,80 @@
 const maindiv = document.querySelector("main");
+const text = document.querySelector(".Info  h2");
+const phoneticse = document.querySelector(".Info  p");
+const Links = document.querySelector("a");
+const Audios = document.querySelector("#song");
+const search = document.querySelector("#Search");
+const BtnShearch = document.querySelector("#BtnShearch");
+const divInfo = document.querySelector("div.Info");
+const footer = document.querySelector("footer");
+const checkbox = document.querySelector("#checkbox");
+const fonte = document.querySelector("#font");
+
+let audio = "";
+let dataList = [];
+let theme = 0;
+let holdsearch = "";
+let searchs = 0;
+
+fonte.addEventListener("input", () => {
+	document.documentElement.style.setProperty(
+		"--font-primary",
+		`${fonte.value}`
+	);
+});
+
+checkbox.addEventListener("input", () => {
+	theme = theme == 1 ? 0 : 1;
+	if (theme == 1) {
+		document.documentElement.style.setProperty("--color-white", "#ffffff");
+		document.documentElement.style.setProperty(
+			"--color-background-primary",
+			"#050505"
+		);
+		document.documentElement.style.setProperty(
+			"--color-background-secondary",
+			"#1f1f1f"
+		);
+	} else {
+		document.documentElement.style.setProperty("--color-white", "#2D2D2D");
+		document.documentElement.style.setProperty(
+			"--color-background-primary",
+			"#ffffff"
+		);
+		document.documentElement.style.setProperty(
+			"--color-background-secondary",
+			"#F4F4F4"
+		);
+	}
+});
+
+Audios.addEventListener("click", () => {
+	audio.play();
+});
+
+document.addEventListener("keydown", function (event) {
+	if (event.key == "Enter") {
+		if (search.value == holdsearch) {
+			searchs++;
+			get(holdsearch, searchs);
+		} else {
+			holdsearch = search.value;
+			searchs = 0;
+			get(holdsearch, searchs);
+		}
+	}
+});
+
+BtnShearch.addEventListener("click", () => {
+	if (search.value == holdsearch) {
+		searchs++;
+		get(holdsearch, searchs);
+	} else {
+		holdsearch = search.value;
+		searchs = 0;
+		get(holdsearch, searchs);
+	}
+});
 
 async function Fetcha(input) {
 	let res = await fetch(
@@ -7,13 +83,45 @@ async function Fetcha(input) {
 	let data = await res.json();
 	return data;
 }
-async function get() {
-	let adata = await Fetcha("tag");
-	Print(adata);
+async function get(word, page) {
+	let temp = document.querySelector("main > p");
+	if (temp) temp.remove();
+
+	for (let i = 0; i < dataList.length; i++) {
+		dataList[i].remove();
+	}
+	let adata = await Fetcha(word);
+	Print(adata, page);
+	dataList = document.querySelectorAll(".meanings");
 }
 
-function Print(data) {
-	console.log(data[0]);
+function Print(data, page) {
+	if (data.title == "No Definitions Found") {
+		divInfo.style.display = "none";
+		footer.style.display = "none";
+		Audios.style.display = "none";
+		const p = document.createElement("p");
+		p.textContent = "No Definitions Found ;(";
+		maindiv.appendChild(p);
+		return;
+	}
+	divInfo.style.display = "flex";
+	footer.style.display = "unset";
+	Audios.style.display = "unset";
+	console.log(data);
+	text.textContent = data[page].word;
+	Links.textContent = data[page].sourceUrls[page];
+	Links.href = data[page].sourceUrls[page];
+	phoneticse.textContent = data[page].phonetics[0].text;
+	audio = "";
+	audio = new Audio(data[page].phonetics[0].audio);
+	for (let i = 0; i < data[page].meanings.length; i++) {
+		Add(
+			data[page].meanings[i].definitions,
+			data[page].meanings[i].partOfSpeech,
+			data[page].meanings[i].synonyms
+		);
+	}
 }
 
 function Add(meanings, tag, Synonyms) {
@@ -38,8 +146,15 @@ function Add(meanings, tag, Synonyms) {
 
 	meanings.forEach((meaning) => {
 		const listItem = document.createElement("li");
-		listItem.textContent = meaning;
+		const p = document.createElement("p");
+		p.textContent = meaning.definition;
 		meaningsList.appendChild(listItem);
+		listItem.appendChild(p);
+		if (meaning.example) {
+			const span = document.createElement("span");
+			span.textContent = `"${meaning.example}"`;
+			listItem.appendChild(span);
+		}
 	});
 
 	meaningsDiv.appendChild(meaningsList);
@@ -48,7 +163,10 @@ function Add(meanings, tag, Synonyms) {
 	const synonymsParagraph = document.createElement("p");
 	synonymsParagraph.textContent = "Synonyms";
 	const synonymText = document.createElement("p");
-	synonymText.textContent = Synonyms;
+
+	Synonyms.forEach((Synonym) => {
+		synonymText.textContent += `${Synonym} / `;
+	});
 
 	if (Synonyms != "") {
 		synonymsDiv.appendChild(synonymsParagraph);
